75b4d6f8559f773a83f182f988c271a480c5a100
code3743 <jota.lopez@correounivalle.edu.co>
2024-09-08 11:53:12 -0500

    refactor: update verify-login.js to use inquirer for user login confirmation

--- src/utils/verify-login.js
+++ src/utils/verify-login.js
@@ -1,9 +1,5 @@
-const readline = require('readline');
+const inquirer = require('inquirer');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 /**
  * Prompts the user to confirm whether they have successfully logged in.
@@ -11,21 +7,24 @@ const rl = readline.createInterface({
  * 
  * @returns {Promise<boolean>} - Resolves to true if the user confirms login with 'y', otherwise false.
  */
-const verifyLogin = () => {
-  return new Promise((resolve) => {
-    console.log('Please log in to the provider page.');
-    console.log('Once you have logged in, press "y" to continue or any other key to cancel.');
-    rl.question('Have you logged in? (y/n): ', (answer) => {
-      if (answer.toLowerCase() === 'y') {
-        console.log('Login confirmed. Continuing...');
-        resolve(true);  
-      } else {
-        console.log('Login not confirmed. Cancelling...');
-        resolve(false); 
-      }
-      rl.close();
-    });
-  });
+const verifyLogin = async () => {
+  const prompt = inquirer.createPromptModule();
+
+ const {login} = await prompt([
+    {
+      type: 'confirm',
+      name: 'login',
+      message: 'Have you logged in to the provider page?',
+      default: false,
+    }
+  ]);
+
+  if (login) {
+    console.log('Login confirmed. Continuing...');
+    return true;
+  }
+  console.log('Login not confirmed. Cancelling...');
+  return false;
 };
 
 module.exports = verifyLogin;
